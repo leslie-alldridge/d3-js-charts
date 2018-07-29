b949d73093cfbda4cf4393435aa00223fbad8883
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-07-30 05:42:23 +1200

    added axis to bar chart
    added axis to bar chart

--- script.js
+++ script.js
@@ -70,32 +70,66 @@
 
 //lesson 4 scalability
 
- var dataset = [1,2,3,4,5];
+//  var dataset = [1,2,3,4,5];
 
- var svgWidth = 500, svgHeight = 300, barPadding = 5;
- var barWidth = (svgWidth / dataset.length);
+//  var svgWidth = 500, svgHeight = 300, barPadding = 5;
+//  var barWidth = (svgWidth / dataset.length);
 
- var svg = d3.select('svg')
-     .attr("width", svgWidth)
-     .attr("height", svgHeight)
+//  var svg = d3.select('svg')
+//      .attr("width", svgWidth)
+//      .attr("height", svgHeight)
 
-//this is the scaling function
-var yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset)])
-    .range([0, svgHeight]);
+// //this is the scaling function
+// var yScale = d3.scaleLinear()
+//     .domain([0, d3.max(dataset)])
+//     .range([0, svgHeight]);
 
-var barChart = svg.selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("y", function(d){
-        return svgHeight - yScale(d)
-    })
-    .attr("height", function(d){
-        return yScale(d);
-    })
-    .attr("width", barWidth - barPadding)
-    .attr("transform", function(d, i){
-        var translate = [barWidth * i, 0];
-        return "translate(" + translate + ")";
-    });
+// var barChart = svg.selectAll("rect")
+//     .data(dataset)
+//     .enter()
+//     .append("rect")
+//     .attr("y", function(d){
+//         return svgHeight - yScale(d)
+//     })
+//     .attr("height", function(d){
+//         return yScale(d);
+//     })
+//     .attr("width", barWidth - barPadding)
+//     .attr("transform", function(d, i){
+//         var translate = [barWidth * i, 0];
+//         return "translate(" + translate + ")";
+//     });
+
+    //lesson 5 axis 
+// var data = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+// var svgWidth = 600, svgHeight = 300, barPadding = 5;
+
+// var svg = d3.select('svg')
+//     .attr("width", svgWidth)
+//     .attr("height", svgHeight)
+
+// //these are the scaling functions
+// var yScale = d3.scaleLinear()
+//     .domain([0, d3.max(data)])
+//     .range([svgHeight, 0]);
+
+// var xScale = d3.scaleLinear()
+//     .domain([0, d3.max(data)])
+//     .range([0, svgWidth]);
+
+// var x_axis = d3.axisBottom()
+//     .scale(xScale);
+// var y_axis = d3.axisLeft()
+//     .scale(yScale);
+
+// svg.append("g")
+//     .attr("transform", "translate(50,10)")
+//     .call(y_axis);
+
+// var xAxisTranslate = svgHeight - 20;
+
+// svg.append("g")
+//     .attr("transform", "translate(50, " + xAxisTranslate + ")")
+//     .call(x_axis);
+
